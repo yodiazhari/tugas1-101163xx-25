994efcd3a6493330e8cd38eee6398c28472509c2
yodiazhari <50961219+yodiazhari@users.noreply.github.com>
2019-10-11 22:20:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<style>
+	div.a {
+	position: absolute;
+	margin-top: -100px;
+	margin-left: -200px;
+	left: 50%;
+	top: 50%;
+	width: 400px;
+	height: 220px;
+}
+</style>
+
+<div class ="a">
+<?php
+	echo "<table border = '2' >";
+	echo "<tr>";
+	echo "<td> NIM </td>" ;
+	echo "<td> 10116307 </td> " ;
+	echo "</tr>";
+	echo "<tr>";
+	echo "<td> Nama </td>"; 
+	echo "<td> Yodi Azhari </td> " ;
+	echo "</tr>";
+	echo "</table>";
+?>
+
+</div>
+	
